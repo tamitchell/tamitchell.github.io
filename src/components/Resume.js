9560f875d07671235b3fe19d62180202addb3943
Tasha Mitchell <tamitchell14@gmail.com>
2018-09-17 10:37:39 -0400

    undid last pull

---
+++ src/components/Resume.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// const Resume = () => {
+//     return (
+//         <iframe title="Resume" src="https://resume.creddle.io/embed/79jkrw15jb8"
+//   width="850" height="1100" seamless></iframe>
+//     )
+// }
+
+// export default Resume
