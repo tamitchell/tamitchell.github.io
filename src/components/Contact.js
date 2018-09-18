8801d7ca06b29227514da5896747813bd65d5362
Tasha Mitchell <tamitchell14@gmail.com>
2018-09-18 16:37:43 -0400

    changed contact header

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@ import {Row} from 'react-materialize'
 
 const Contact = () => (
   <Row className="contact-container">
-    <h2>Contact</h2>
+    <h1>Contact</h1>
     <p>To view full resume, click <a href="https://www.dropbox.com/s/iagxfcd07e8by8h/2tashamitchell_resume.pdf?dl=0">here</a></p>
     <p>For all business inquiries, email me at <a href="mailto:tamitchell14@gmail.com">tamitchell14@gmail.com</a></p>
   </Row>
