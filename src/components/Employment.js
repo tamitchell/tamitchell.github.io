9efe9e78c896693b4c14c08d5d1cab89e4b4a381
Tasha Mitchell <tamitchell14@gmail.com>
2018-11-23 13:28:13 -0500

    updated employment and resume

--- src/components/Employment.js
+++ src/components/Employment.js
@@ -6,6 +6,44 @@ const Employment = () => (
     <h1>Employment</h1>
     <Row>
       <Col s={12} m={4} l={4}>
+        <a href="http://www.metistream.com/">
+          <img
+            src={require("../img/metistream_logo.png")}
+            alt="MetiStream Logo"
+            title="MetiStream"
+          />
+        </a>
+      </Col>
+      <Col s={12} m={8} l={8}>
+        <h5>MetiStream</h5>
+        <h6>
+          Graphic Designer
+          <br />
+          November 2018
+        </h6>
+
+        <p>
+          MetiStream is a healthcare analytics platform and solutions provider.
+          We deliver innovation at the intersection of big data, artificial
+          intelligence and healthcare analytics.
+          <br />
+          <br />
+          My role as a part-time Graphic
+          Designer for MetiStream was to help improve preexisting marketing and pitch deck
+          material and redesign them in a modern and minimalistic way. To
+          achieve this, I got to work hands on with the CEO and COO of the
+          company to ensure graphic accuracy and high quality consistency were
+          maintain over the tranformation of the marketing deck by creating
+          graphics, icons, slide templates and other PPT materials.
+        </p>
+        <p>
+          Technologies Used: Adobe Illustrator, Photoshop, Autodesk Sketchbook
+          Pro, Google Slides, Microsoft Powerpoint
+        </p>
+      </Col>
+    </Row>
+    <Row>
+      <Col s={12} m={4} l={4}>
         <a href="http://srtlabs.com/">
           <img
             src={require("../img/srt.png")}
@@ -17,28 +55,27 @@ const Employment = () => (
       <Col s={12} m={8} l={8}>
         <h5>Service Robotics &amp; Technologies</h5>
         <h6>
-          Software Developer
+          Front End Software Developer
           <br />
           October 2018 - Present
         </h6>
         <p>
           Service Robotics Technologies, Inc. (SRT) is a Virginia-based service
           robotics integration company bringing smart building systems to
-          service industry businesses and commercial facilities. 
+          service industry businesses and commercial facilities.
           <br />
           <br />
-          My role as a
-          Software Developer focused on improving user experience as well
-          debugging several errors within the ReactJS framework so that project could successful
-          move from a 'beta' project to their 'MVP' that they needed to present
-          at a conference by the end of the month.
+          My role as a Front End Software Developer focused on improving user experience
+          as well debugging several errors within the ReactJS framework so that
+          project could successfully move from a 'beta' project to their 'MVP'
+          that they needed to present at a conference by the end of the month.
         </p>
         <p>Technologies Used: ReactJS, PHP, Symphony, JavaScript(ES6), CSS3 </p>
       </Col>
     </Row>
     <Row>
       <Col s={12} m={4} l={4}>
-        <a href="http://happytail-dev.surge.sh">
+        <a href="http://www.happytailva.com/">
           <img
             src={require("../img/happytail.png")}
             alt="Happy Tail Logo"
@@ -60,8 +97,8 @@ const Employment = () => (
           flexible layout optimal for mobile, tablet, and desktop view.
         </p>
         <p>
-          Technologies Used: JavaScript(ES6), HTML5 + CSS, SASS, Bootstrap, Surge (for
-          deployment)
+          Technologies Used: JavaScript(ES6), HTML5 + CSS, SASS, Bootstrap,
+          Surge (for deployment)
         </p>
       </Col>
     </Row>
