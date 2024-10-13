31cc111b065f80667d553b2b01ed72facd2851bb
Tasha <24355829+tamitchell@users.noreply.github.com>
2024-10-12 21:01:59 -0600

    updating with latest

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/tamitchell/tamitchell.github.io",
+  base: '/',
   resolve: {
     alias: {
       '@components': path.resolve(__dirname, './src/components'),
